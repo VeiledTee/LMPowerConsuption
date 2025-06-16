1c4d1953d52b3a24c17c740f933d1d4178c259a6
penguins <ethanheavey@outlook.com>
2025-06-16 10:17:16 -0300

    scoring file

---
+++ src/scorers.py
@@ -0,0 +1,14 @@
+import re
+from utils import normalize
+
+def exact_match(pred: str, gold: str) -> int:
+    return int(normalize(pred) == normalize(gold))
+
+def f1_score(pred: str, gold: str) -> float:
+    pt, gt = normalize(pred).split(), normalize(gold).split()
+    common = set(pt) & set(gt)
+    if not common:
+        return 0.0
+    prec = len(common) / len(pt)
+    rec = len(common) / len(gt)
+    return 2 * prec * rec / (prec + rec)
