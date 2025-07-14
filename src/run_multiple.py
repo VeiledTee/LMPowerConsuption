36a9542ab78c94d380938395a1faec6c79f8b66b
Veiled Tee <ethanheavey@outlook.com>
2025-07-14 16:14:23 -0300

    add analysis after processing

--- src/run_multiple.py
+++ src/run_multiple.py
@@ -1,6 +1,7 @@
 from config import CONFIG
 from experiment import run
 import logging
+from analysis import main
 
 logger = logging.getLogger(__name__)
 
@@ -13,10 +14,15 @@ def safe_run(tag):
         logger.exception(f"Run {tag} failed: {e}")
 
 
-# First config
-CONFIG.think = True
-safe_run("deepseek-big-think")
+# # First config
+# CONFIG.think = True
+# safe_run("deepseek-big-think")
+main('deepseek')
 
 # Second config
+CONFIG.model_types={"gemma3:4b": "ollama","gemma3:12b": "ollama","gemma3:27b": "ollama",}
+CONFIG.modes={"gemma3:4b": {"q": False, "q+r": True},"gemma3:12b": {"q": False, "q+r": True},"gemma3:27b": {"q": False},}
 CONFIG.think = False
-safe_run("deepseek-smooth-brain")
+safe_run("gemma3-big")
+
+main('gemma3')
