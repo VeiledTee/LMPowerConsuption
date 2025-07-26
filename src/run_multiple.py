7952812ae5f55c9047bdd6013c8d0a984bd2c05e
Veiled Tee <ethanheavey@outlook.com>
2025-07-26 09:28:51 -0300

    gemma3 testing

--- src/run_multiple.py
+++ src/run_multiple.py
@@ -6,31 +6,28 @@ from analysis import main
 logger = logging.getLogger(__name__)
 
 
-def safe_run(tag):
+def safe_run(tag, file_suffix: None | str = ""):
     logger.info(f"Starting run: {tag}")
     try:
-        run()
+        run(file_suffix)
     except Exception as e:
         logger.exception(f"Run {tag} failed: {e}")
 
 
-# First config
-CONFIG.think = True
-safe_run("deepseek-think")
-# main("_deepseek")
+# # First config
+# CONFIG.think = True
+# safe_run("deepseek-think")
+# # main("_deepseek")
 
 # Second config
 # CONFIG.model_types = {
 #     "gemma3:4b": "ollama",
-#     "gemma3:12b": "ollama",
-#     "gemma3:27b": "ollama",
 # }
 # CONFIG.modes = {
-#     "gemma3:4b": {"q": False, "q+r": True},
-#     "gemma3:12b": {"q": False, "q+r": True},
-#     "gemma3:27b": {"q": False},
+#     "gemma3:4b": {"q+r": True},
 # }
 CONFIG.think = False
-safe_run("deepseek-no-think")
+for i in range(5):
+    safe_run("gemma3_dev_testing", str(i))
 
-main("hotpot_deepseek")
+# main("hotpot_deepseek")
