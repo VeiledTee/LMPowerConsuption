8da84be1975cb3906b1d9f40811d57b89d30650a
Veiled Tee <ethanheavey@outlook.com>
2025-12-12 15:32:43 -0400

    rerun qwen3 hotpot

--- src/run_multiple.py
+++ src/run_multiple.py
@@ -42,50 +42,64 @@ CONFIG.modes = {
     "qwen3:14b": {"q": False, "q+r": True},
     "qwen3:32b": {"q": False},
  }
-
-# First config
-CONFIG.think = True
-CONFIG.gold = True
-safe_run("qwen3-hotpot-think")
-send_email(
-    from_addr=os.getenv("EMAIL_FROM", CONFIG.from_email),
-    to_addr=CONFIG.to_email,
-    subject="Completed qwen3-hotpot with no issues.",
-    body="No errors",
-)
-
-# Second config
+# 1st config
 CONFIG.think = False
 CONFIG.gold = True
-safe_run("qwen3-hotpot-nothink")
-send_email(
-    from_addr=os.getenv("EMAIL_FROM", CONFIG.from_email),
-    to_addr=CONFIG.to_email,
-    subject="Completed qwen3-hotpot with no issues.",
-    body="No errors",
-)
+try:
+    safe_run("qwen3-hotpot-nothink")
+    send_email(
+        from_addr=os.getenv("EMAIL_FROM", CONFIG.from_email),
+        to_addr=CONFIG.to_email,
+        subject="Completed qwen3-hotpot no think with no issues.",
+        body="No errors",
+    )
+except Exception as error:
+    send_email(
+        from_addr=os.getenv("EMAIL_FROM", CONFIG.from_email),
+        to_addr=CONFIG.to_email,
+        subject="Script Error",
+        body=f"The qwen3-hotpot-nothink script crashed with:\n\n{error}",
+    )
 
-
-CONFIG.model_types = {
-    "deepseek-r1:1.5b": "ollama",
-    "deepseek-r1:7b": "ollama",
-    "deepseek-r1:14b": "ollama",
-    "deepseek-r1:32b": "ollama"
-}
-CONFIG.modes = {
-    "deepseek-r1:1.5b": {"q": False, "q+r": True},
-    "deepseek-r1:7b": {"q": False, "q+r": True},
-    "deepseek-r1:14b": {"q": False, "q+r": True},
-    "deepseek-r1:32b": {"q": False}
- }
-
-# First config
+# 2nd config
 CONFIG.think = True
 CONFIG.gold = True
-safe_run("deepseek-hotpot")
-send_email(
-    from_addr=os.getenv("EMAIL_FROM", CONFIG.from_email),
-    to_addr=CONFIG.to_email,
-    subject="Completed deepseek-hotpot with no issues.",
-    body="No errors",
-)
+try:
+    safe_run("qwen3-hotpot-think")
+    send_email(
+        from_addr=os.getenv("EMAIL_FROM", CONFIG.from_email),
+        to_addr=CONFIG.to_email,
+        subject="Completed qwen3-hotpot think with no issues.",
+        body="No errors",
+    )
+except Exception as error:
+    send_email(
+        from_addr=os.getenv("EMAIL_FROM", CONFIG.from_email),
+        to_addr=CONFIG.to_email,
+        subject="Script Error",
+        body=f"The qwen3-hotpot-think script crashed with:\n\n{error}",
+    )
+
+# CONFIG.model_types = {
+#     "deepseek-r1:1.5b": "ollama",
+#     "deepseek-r1:7b": "ollama",
+#     "deepseek-r1:14b": "ollama",
+#     "deepseek-r1:32b": "ollama"
+# }
+# CONFIG.modes = {
+#     "deepseek-r1:1.5b": {"q": False, "q+r": True},
+#     "deepseek-r1:7b": {"q": False, "q+r": True},
+#     "deepseek-r1:14b": {"q": False, "q+r": True},
+#     "deepseek-r1:32b": {"q": False}
+#  }
+#
+# # First config
+# CONFIG.think = True
+# CONFIG.gold = True
+# safe_run("deepseek-hotpot")
+# send_email(
+#     from_addr=os.getenv("EMAIL_FROM", CONFIG.from_email),
+#     to_addr=CONFIG.to_email,
+#     subject="Completed deepseek-hotpot with no issues.",
+#     body="No errors",
+# )
